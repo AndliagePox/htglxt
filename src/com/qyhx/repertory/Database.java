e2e5edf3543a0e41ebb1ba3e5b676f546f6c0120
Andliage <pox@andliage.com>
2019-12-30 16:22:52 +0800

    数据库配置完善

--- src/com/qyhx/repertory/Database.java
+++ src/com/qyhx/repertory/Database.java
@@ -17,6 +17,7 @@ class Database {
     static Connection getConnection() {
         try {
             if (connection == null || connection.isClosed()) {
+                loadConfig();
                 Class.forName(driver);
                 connection = DriverManager.getConnection(
                         "jdbc:mysql://localhost:3306/" + database + "?useSSL=false&serverTimezone=GMT%2B8",
@@ -30,16 +31,50 @@ class Database {
         return connection;
     }
 
-    private void loadConfig() {
+    private static void loadConfig() {
         try {
+            int index;
             String line;
             File file = new File("database.cfg");
             BufferedReader reader = new BufferedReader(new FileReader(file));
             while ((line = reader.readLine()) != null) {
                 line = line.replace("\n", "");
                 line = line.replace(" ", "");
-                int index = line.indexOf("driver=");
-
+                index = line.indexOf("driver=");
+                if (index != -1) {
+                    line = line.replace("driver=", "");
+                    index = line.indexOf(";");
+                    line = line.substring(0, index);
+                    driver = line;
+                    System.out.println(driver);
+                    continue;
+                }
+                index = line.indexOf("database=");
+                if (index != -1) {
+                    line = line.replace("database=", "");
+                    index = line.indexOf(";");
+                    line = line.substring(0, index);
+                    database = line;
+                    System.out.println(driver);
+                    continue;
+                }
+                index = line.indexOf("username=");
+                if (index != -1) {
+                    line = line.replace("username=", "");
+                    index = line.indexOf(";");
+                    line = line.substring(0, index);
+                    username = line;
+                    System.out.println(driver);
+                    continue;
+                }
+                index = line.indexOf("password=");
+                if (index != -1) {
+                    line = line.replace("password=", "");
+                    index = line.indexOf(";");
+                    line = line.substring(0, index);
+                    password = line;
+                    System.out.println(driver);
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
