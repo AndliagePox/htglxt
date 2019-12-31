2d162b8573909a9bac38713803b660ef7aec489a
Andliage <pox@andliage.com>
2019-12-31 15:39:56 +0800

    HT update

--- src/com/qyhx/controller/HTController.java
+++ src/com/qyhx/controller/HTController.java
@@ -1,9 +1,12 @@
 package com.qyhx.controller;
 
+import com.qyhx.entity.HT;
 import com.qyhx.printer.HTPrinter;
 import com.qyhx.repertory.HTRepertory;
 
-public class HTController extends BaseController implements ControllerInterface{
+import java.util.Scanner;
+
+public class HTController extends BaseController implements ControllerInterface {
     @Override
     public void showAll() {
         HTRepertory htRepertory = new HTRepertory();
@@ -12,7 +15,26 @@ public class HTController extends BaseController implements ControllerInterface{
 
     @Override
     public void updateOne() {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("输入要更新的合同id：");
+        int id = scanner.nextInt();
+        HTRepertory htRepertory = new HTRepertory();
+        HT ht = htRepertory.findOneById(id);
+        if (ht == null) {
+            System.out.println("未查询到该合同");
+            return;
+        }
+        System.out.print("输入更新后的合同名称：");
+        ht.setName(scanner.nextLine());
+        System.out.print("输入更新后的合同编号：");
+        ht.setNumber(scanner.nextLine());
+        System.out.println("输入更新后的合同签订时间(格式:yyyy-mm-dd HH:ii:ss)：");
+        ht.setqTime(scanner.nextLine());
+        System.out.print("输入合同对应客户的客户id：");
+        ht.setKhId(scanner.nextInt());
+        System.out.print("输入合同对应项目的项目id：");
+        ht.setXmId(scanner.nextInt());
+        htRepertory.updateOne(ht);
     }
 
     @Override
--- src/com/qyhx/repertory/HTRepertory.java
+++ src/com/qyhx/repertory/HTRepertory.java
@@ -28,4 +28,27 @@ public class HTRepertory extends BaseRepertory {
         }
         return list;
     }
+
+    public HT findOneById(int id) {
+        try {
+            ResultSet rs = executeSelect("select * from htb where id = " + id);
+            if (rs.next())
+                return new HT(rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("number"),
+                        rs.getString("qtime"),
+                        rs.getInt("kh_id"),
+                        rs.getInt("xm_id"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return null;
+    }
+
+    public void updateOne(HT ht) {
+        String sql = String.format("update from htb set name = '%s', number = '%s', qtime = '%s', kh_id = %d, xm_id = %d where id = %d",
+                ht.getName(), ht.getNumber(), ht.getqTime(), ht.getKhId(), ht.getXmId(), ht.getId());
+        executeUpdate(sql);
+    }
 }
